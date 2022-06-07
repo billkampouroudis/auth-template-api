daf891826b98ea58efd0fa6e230b9bb8ae36a08b
Bill Kampouroudis <v.kampouroudis@gmail.com>
2022-06-07 23:02:55 +0300

    Add i18next

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 import express from 'express';
 import { urlencoded, json } from 'body-parser';
 import cors from 'cors';
+import i18next from 'i18next';
+import i18nextMiddleware from 'i18next-http-middleware';
+import i18nextBackend from 'i18next-fs-backend';
 import initDatabase from './src/config/database';
 import initPassport from './src/config/passport';
 import errorHandling from './src/middlewares/errorHandling';
@@ -20,6 +23,24 @@ initPassport();
 app.use(cors());
 
 // app.use(express.static('public'));
+
+i18next
+  .use(i18nextBackend)
+  .use(i18nextMiddleware.LanguageDetector)
+  .init({
+    lookupQuerystring: 'lng',
+    backend: {
+      // eslint-disable-next-line no-path-concat
+      loadPath: `${__dirname}/src/locales/{{lng}}/{{ns}}.json`,
+      // eslint-disable-next-line no-path-concat
+      addPath: `${__dirname}/src/locales/{{lng}}/{{ns}}.missing.json`
+    },
+    fallbackLng: 'en'
+  });
+
+app.use(
+  i18nextMiddleware.handle(i18next)
+);
 
 (async () => {
   await initDatabase();
